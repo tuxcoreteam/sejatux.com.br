f418987f0893e99eb887a0db02b546a729d28e6a
Flavio Liberato <flavioberato@gmail.com>
2020-03-04 22:29:50 -0300

    👑

--- templates/script.js
+++ templates/script.js
@@ -1,24 +1,37 @@
-$(document).ready(function() {
-  $("#myCarousel").on("slide.bs.carousel", function(e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $(".carousel-item").length;
+$(function() {
 
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(".carousel-item")
-            .eq(i)
-            .appendTo(".carousel-inner");
-        } else {
-          $(".carousel-item")
-            .eq(0)
-            .appendTo($(this).find(".carousel-inner"));
-        }
-      }
-    }
+  "use strict"
+  
+  var init = "No items yet!";
+  var counter = 0;
+
+  // Initial Cart
+  $(".counter").html(init);
+  
+  // Add Items To Basket
+  function addToBasket() {
+    counter++;
+    $(".counter").html(counter).animate({
+      'opacity' : '0'
+    },300, function() {
+      $(".counter").delay(300).animate({
+        'opacity' : '1'
+      })
+    })
+  }
+
+  // Add To Basket Animation
+  $("button").on("click", function() {
+    addToBasket(); $(this).parent().parent().find(".product_overlay").css({
+      'transform': ' translateY(0px)',
+      'opacity': '1',
+      'transition': 'all ease-in-out .45s'
+    }).delay(1500).queue(function() {
+      $(this).css({
+        'transform': 'translateY(-500px)',
+        'opacity': '0',
+        'transition': 'all ease-in-out .45s'
+      }).dequeue();
+    });
   });
 });
